ccd74ee1cd676f9e8c3554cca0dd23ac0dcf8583
Sathya <neelasathya06@gmail.com>
2025-07-19 17:34:13 -0400

    arrays and for loop

---
+++ app/src/main/java/dev/sathya/mylearning/ArraysDatatypes.kt
@@ -0,0 +1,24 @@
+package dev.sathya.mylearning
+
+
+fun main(args: Array<String>){
+
+    var age = arrayOf(24, 14,8, 12,16,19, 36)
+
+
+    var carsAndAge = arrayOf("Mercedes", "BMW", "Opel", "BMW", 10)
+
+
+    for (e in carsAndAge){
+        println(e)
+    }
+
+//    println("First ELement of Array = " + cars.get(0))
+//    println("Second ELement of Array = " + cars.get(1))
+//    println("Third ELement of Array = " + cars.get(2))
+//
+//
+//    var carSet = cars.toSet()
+//    println(carSet)
+
+}
