88195a5d6156888b412d2259fed1bd8d60b0c300
Sathya <neelasathya06@gmail.com>
2025-07-19 17:12:25 -0400

    var  & val datatypes

---
+++ app/src/main/java/dev/sathya/mylearning/Variables.kt
@@ -0,0 +1,23 @@
+package dev.sathya.mylearning
+
+fun main(args: Array<String>){
+    var a = 10;
+    var b = 20;
+    var c = a + b;
+
+    println("The result is : $c")
+
+    var age = 10;
+    var _age = 20;
+    var age3 = 30;
+
+//    var 3age = 34;
+
+    age = 50;
+    println(age)
+
+    val year = 2020;
+//    year = 2019;
+
+
+}
