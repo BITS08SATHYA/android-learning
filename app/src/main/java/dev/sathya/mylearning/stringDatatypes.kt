b83cdcbe110dd4e1bc94060cc5b61e1720f97310
Sathya <neelasathya06@gmail.com>
2025-07-19 17:26:58 -0400

    string datatypes

---
+++ app/src/main/java/dev/sathya/mylearning/stringDatatypes.kt
@@ -0,0 +1,32 @@
+package dev.sathya.mylearning
+
+fun main(args: Array<String>){
+
+    var a = "Hello"
+    var b = "Kotlin"
+
+    // length
+//    equals(another object)
+//    isEmpty()
+//    plus("")
+//    lowercase()
+//    uppercase()
+//    trim()
+
+    var size = a.length
+    var comp = a.equals(b)
+    var emp = a.isEmpty()
+    var plus = a + " " + b
+    var upper = a.uppercase()
+    var lower = a.lowercase()
+
+    println(size)
+    println(comp)
+    println(emp)
+    println(plus)
+    println(upper)
+    println(lower)
+
+
+
+}
