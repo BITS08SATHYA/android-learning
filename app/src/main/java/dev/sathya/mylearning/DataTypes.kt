7ecc6715cf6e4aec866970ffeaf9b43bd5c1d0e8
Sathya <neelasathya06@gmail.com>
2025-07-19 17:16:54 -0400

    datatypes declared

---
+++ app/src/main/java/dev/sathya/mylearning/DataTypes.kt
@@ -0,0 +1,22 @@
+package dev.sathya.mylearning
+
+fun main(args: Array<String>){
+    var a = true
+    var b = 'R'
+    var c = 12
+    var d = -356
+    var e = 43543
+    var f = -432443234L
+    var g = 5.68764324F
+    var h = 7.34544276464
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
+    println(h)
+
+}
