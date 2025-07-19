95328fef2e9f81f9a82f4d9646cf08a9f4a0a810
Sathya <neelasathya06@gmail.com>
2025-07-19 17:21:27 -0400

    type conversion completed

---
+++ app/src/main/java/dev/sathya/mylearning/TypeConversion.kt
@@ -0,0 +1,22 @@
+package dev.sathya.mylearning
+
+fun main(args: Array<String>){
+
+//    var x = 127;
+//    var y = x.toInt();
+//    var z = y.toDouble();
+//
+//    println(x)
+//    println(y)
+//    println(z)
+
+
+    var a = 132.32
+    var b = a.toInt()
+    var c = b.toByte()
+
+    println(a)
+    println(b)
+    println(c)
+
+}
